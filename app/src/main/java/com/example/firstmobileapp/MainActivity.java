5edf23e4a0c0667f8704975ccd95f3b258922982
FlashLLan <lasnkyalex888776@gmail.com>
2024-09-16 09:25:03 +0300

    commir message and push

--- app/src/main/java/com/example/firstmobileapp/MainActivity.java
+++ app/src/main/java/com/example/firstmobileapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         // find the button and text view by their IDs
         Button myButton = findViewById(R.id.button);
         final TextView myTextView = findViewById(R.id.textView);
+        Button mySecondButton = findViewById(R.id.button2);
+
 
         final String initialText = "The text before you press the button";
         final String changedText = "Text after you pressed the button";
@@ -23,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         // here we set the initial text
         myTextView.setText(initialText);
 
-        // here is functionality for the button
+        // here is functionality for the first button
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,6 +34,19 @@ public class MainActivity extends AppCompatActivity {
                     myTextView.setText(changedText);
                 } else {
                     myTextView.setText(initialText);
+                }
+            }
+        });
+
+        // here is functionality for the second button
+        mySecondButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toggle text color based on current color
+                if (myTextView.getCurrentTextColor() == ContextCompat.getColor(MainActivity.this, R.color.text_initial_color)) {
+                    myTextView.setTextColor(ContextCompat.getColor(MainActivity.this, R.color.text_changed_color));
+                } else {
+                    myTextView.setTextColor(ContextCompat.getColor(MainActivity.this, R.color.text_initial_color));
                 }
             }
         });
